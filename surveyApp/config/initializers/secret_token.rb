32852487c3089b8b82510550b8061661f5d79749
Seth Robbins <robbins.sd@gmail.com>
2013-06-24 07:20:49 -0500

    first commit

---
+++ surveyApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SurveyApp::Application.config.secret_key_base = 'fb9e7379d2ad9622a4d3d9a2c36ba2d8fca81e3c0b9b0dcd32197aa1b38fea8db6842ca275aa3efd8db9dae68dacdaeaadb2e685dba5d2db2eb08ee9b4f80348'
